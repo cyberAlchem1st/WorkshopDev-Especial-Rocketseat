7f8fbfedbd37e52932a8c7e743f3b26874584069
Richard Bruno <richard.ufabc@gmail.com>
2020-04-16 19:59:16 -0300

    Uploaded files

---
+++ public/main.js
@@ -0,0 +1,45 @@
+function onoff() {
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+
+    document
+    .querySelector("body")
+    .classList
+    .toggle("hideScroll")
+
+    document
+    .querySelector("#modal")
+    .classList
+    .toggle("addScroll")
+};
+
+function checkFields(event) {
+    //ex: acessar o valor do titulo event.target.["titlle"].value
+    //target: formulario
+
+    const valuesToCheck = [
+        "title",
+        "image",
+        "category",
+        "description",
+        "link"
+    ]
+
+    const isEmpty = valuesToCheck.find(function(value) {
+        const checkIfIsString = typeof event.target[value].value === "string"
+        const checkIfIsEmpty = !event.target[value].value.trim()
+        
+        if(checkIfIsString && checkIfIsEmpty) {
+            return true
+        }
+    })
+
+    if(isEmpty) {
+        event.preventDefault() //não realize o comportamento padrão do código
+        alert("Preencha todos os campos.")
+    }
+
+    
+}
